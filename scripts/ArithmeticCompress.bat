6ca91fb1ddf78e5e60273426907fd1a733ca3309
Connor-Devitt <connordevitt@hotmail.com>
2014-11-09 15:04:04 -0500

    Compressed one file (.txt) into another (.bin)
    *removed the binary dump since it is now needed

--- scripts/ArithmeticCompress.bat
+++ scripts/ArithmeticCompress.bat
@@ -4,5 +4,4 @@ cd project/bin
 
 set file=%1
 set file2=%2
-java Arithmetic/AdaptiveArithmeticCompress files/%file%.txt files/%file2%
-java Algorithms/BinaryDump 32 < files/%file2%
+java Arithmetic/AdaptiveArithmeticCompress files/%file%.txt files/%file2%.bin
