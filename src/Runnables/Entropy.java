370b4deb1b50a516a1e06c4c667b9f758e05af38
Connor-Devitt <connordevitt@hotmail.com>
2014-11-10 18:19:23 -0500

    now accepts a file instead of a string

--- src/Runnables/Entropy.java
+++ src/Runnables/Entropy.java
@@ -1,51 +1,68 @@
 package Runnables;
 
 import java.util.*;
+import java.io.*;
 
 public class Entropy {
-	static double getEntroy(String message) {
-		Vector alphabet = new Vector();
-		double probability[];
-		
-		//add unique characters
-		for(int i = 0; i < message.length(); i++) {
-			if(alphabet.size() == 0) {
-				alphabet.add(message.charAt(0));
+	static double getEntroy(File file) {
+		try {
+			Vector alphabet = new Vector();
+			double probability[];
+			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String message = "";
+			while(br.ready()) {
+				message += br.readLine();
 			}
-			else if(!alphabet.contains(message.charAt(i))) {
-				alphabet.add(message.charAt(i));
-			}
-		}
-		
-		//initialize probability
-		probability = new double[alphabet.size()];
-		for(int i = 0; i < probability.length; i++) {
-			probability[i] = 0;
-		}
-		
-		//adds occurances of each unique character to probabiltiy
-		for(int i = 0; i < message.length(); i++) {
-			for(int j = 0; j < alphabet.size(); j++) {
-				if(alphabet.elementAt(j).toString().equals(message.charAt(i) + "")) {
-					probability[j]++;
+			
+			//add unique characters
+			for(int i = 0; i < message.length(); i++) {
+				if(alphabet.size() == 0) {
+					alphabet.add(message.charAt(0));
+				}
+				else if(!alphabet.contains(message.charAt(i))) {
+					alphabet.add(message.charAt(i));
 				}
 			}
+			
+			//initialize probability
+			probability = new double[alphabet.size()];
+			for(int i = 0; i < probability.length; i++) {
+				probability[i] = 0;
+			}
+			
+			//adds occurances of each unique character to probabiltiy
+			for(int i = 0; i < message.length(); i++) {
+				for(int j = 0; j < alphabet.size(); j++) {
+					if(alphabet.elementAt(j).toString().equals(message.charAt(i) + "")) {
+						probability[j]++;
+					}
+				}
+			}
+			
+			//compute probability and Entropy
+			for(int i = 0; i < probability.length; i++) {
+				probability[i] /= message.length();
+			}
+			
+			//compute entropy
+			double Entropy = 0;
+			for(int i = 0; i < probability.length; i++) {
+				Entropy += probability[i] * logBase(2, (1 / probability[i]));
+			}
+			
+			return Entropy;
 		}
-		
-		//compute probability and Entropy
-		for(int i = 0; i < probability.length; i++) {
-			probability[i] /= message.length();
+		catch(Exception e) {
+			e.printStackTrace();
+			return 0;
 		}
-		
-		//compute entropy
-		double Entropy = 0;
-		for(int i = 0; i < probability.length; i++) {
-			Entropy += probability[i] * logBase(2, (1 / probability[i]));
-		}
-		
-		return Entropy;
 	}
 	static double logBase(int base, double x) {
 		return (Math.log(x) / Math.log(base));
+	}
+	public static void main(String args[]) {
+		Entropy.getEntroy(new File(args[0]));
 	}
 }
