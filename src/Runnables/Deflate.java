5187c556eecf26420fd531745c393054cf9c8ee0
Connor-Devitt <connordevitt@hotmail.com>
2014-11-11 21:57:57 -0500

    NOT CONFIDENT see extended description
    changed the program so it writes a header on the data. I am not sure if this program works as it should. I will test and make corrections ASAP.

--- src/Runnables/Deflate.java
+++ src/Runnables/Deflate.java
@@ -10,10 +10,8 @@ import java.io.*;
  * to decompress back into a string: String outputString = new String(result, 0, resultLength, "UTF-8"); 
 */
 public class Deflate  {
-	private static int compressedDataLength = 0;
-	private static byte[] output;
-	private static int length = 0;
 	public static void compress(BufferedInputStream in, BufferedOutputStream out) {
+		int compressedLength;
 		try {
 			//make string of data and convert to bits
 			String message = "";
@@ -22,22 +20,26 @@ public class Deflate  {
 			}
 			
 			byte[] input = message.getBytes();
-			length = input.length;
+			
 			//compresses the data
 			Deflater compressor = new Deflater();
 			compressor.setInput(input);
 			compressor.finish();
 			
 			byte[] temp = new byte[input.length];
-			compressedDataLength = compressor.deflate(temp);
+			compressedLength = compressor.deflate(temp);
 			
-			output = new byte[compressedDataLength];
+			byte[] output = new byte[compressedLength];
 			for(int i = 0; i < output.length; i++) {
 				output[i] = temp[i];
 			}
 			
 			compressor.end();
 			
+			//writes the head for the length of the compressed data
+			out.write(Integer.toBinaryString(compressedLength).getBytes());
+			//writes the original length of the message
+			out.write(Integer.toBinaryString(input.length).getBytes());
 			//outputs the data
 			out.write(input);
 			
@@ -48,7 +50,20 @@ public class Deflate  {
 	}
 	public static void expand(BufferedInputStream in, BufferedOutputStream out) {
 		try {
+			//reads the header
+			byte[] header1 = new byte[32]; //ints are 32 bits
+			in.read(header1);
+			byte[] header2 = new byte[32]; // ^^
+			in.read(header2);
+			
+			//sets the header to int values
+			int length = Integer.parseInt(new String(header2, "UTF-8"), 2);
+			int compressedDataLength = Integer.parseInt(new String(header1, "UTF-8"), 2);
+			
+			//decompresses
 			Inflater decompresser = new Inflater();
+			byte[] output = new byte[compressedDataLength];
+			in.read(output);
 		    decompresser.setInput(output, 0, compressedDataLength);
 		    
 		    byte[] result = new byte[length];
